5c52018b5778d14ceadeb5f754254b30229e6469
Thibault G <thibault.gerardin@gmail.com>
2024-04-26 12:36:41 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Projet final de la formation développeur web d'Openclassroom : créer et publier un site portfolio. Projet réalisé en React avec Create React App.
--- src/components/Banner/index.jsx
+++
@@ -1,7 +0,0 @@
-import '../../styles/components/Banner.scss'
-
-function Banner(props) {
-  return <div className="Banner">{props.children}</div>
-}
-
-export default Banner
